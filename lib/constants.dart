674ec8f28702d90a3c414e31d3841b1257892c97
aydasameh44 <ayda3854@gmail.com>
2022-11-10 21:52:29 +0200

    NEW

--- lib/constants.dart
+++ lib/constants.dart
@@ -7,4 +7,4 @@ const kTextColor = Color.fromARGB(255, 0, 0, 0);
 const kTextLightColor = Color(0xFF747474);
 const kBlueColor = Color.fromARGB(255, 77, 168, 204);
 const kDefaultPadding = 20.0;
-const currentWidth = Color.fromARGB(255, 40, 103, 238);
+const currentWidth = Color.fromARGB(255, 39, 92, 207);
